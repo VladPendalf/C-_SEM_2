c4b47a5eb039a554ab3696809c24bee58270278b
VladislavPendalf <69432124+VladPendalf@users.noreply.github.com>
2021-05-19 14:21:07 +0300

    Update OOP6.cpp

--- OOP6.cpp
+++ OOP6.cpp
@@ -6,46 +6,39 @@
 //------------------ТЗ---------------------//
 /*
 1 Реализовать структуру для хранения представленных в задании данных (класс, представляющий Связный список, является шаблоном класса, в main его работа
-демонстрируется на примере хранения объекта структуры/класса из Вашего варианта). 
-	Информация в элемент структуры вводится со стандартного потока ввода (предусмотреть автоматическое заполнение). 
-
-2 В классе Связный Список ввести деструктор. 
-	-Поля класса должны быть защищёнными (protected). 
-	-Ввести открытый интерфейс (public) для работы с ними. 
-	-Унаследовать класс списка от базового класса Связный список с ключевым словом public. 
-	-Переопределить функции push() и pop() – для добавления и извлечения элементов из списка. 
-	-Написать функцию вставки нового элемента в произвольное место списка insert(), удаления remove(),  | поиска элемента find() по значению | {что должна делать эта функция?} 
+демонстрируется на примере хранения объекта структуры/класса из Вашего варианта).
+	Информация в элемент структуры вводится со стандартного потока ввода (предусмотреть автоматическое заполнение).
+2 В классе Связный Список ввести деструктор.
+	-Поля класса должны быть защищёнными (protected).
+	-Ввести открытый интерфейс (public) для работы с ними.
+	-Унаследовать класс списка от базового класса Связный список с ключевым словом public.
+	-Переопределить функции push() и pop() – для добавления и извлечения элементов из списка.
+	-Написать функцию вставки нового элемента в произвольное место списка insert(), удаления remove(),  | поиска элемента find() по значению | {что должна делать эта функция?}
 	-Реализовать указанный вид поиска: под функцией фильтра предполагается поиск элементов, которые удовлетворяют условию element.field >, < value, под поиском – только
-element.info==value (element.info – поле info в элементе списка element, value – искомое значение). 
+element.info==value (element.info – поле info в элементе списка element, value – искомое значение).
 	-Фильтр возвращает новый список элементов, удовлетворяющих условию.
-	-Функцию фильтра и поиска реализовать в итеративной и рекурсивной формах. 
+	-Функцию фильтра и поиска реализовать в итеративной и рекурсивной формах.
 	-Класс, объекты которого хранятся в списке, указан в таблице 1
-
 3 (*) Кроме фильтра по указанным полям, написать универсальный фильтр по произвольному полю, который принимает указатель на функцию. Эта функция
 возвращает 1, если элемент удовлетворяет условию и 0 – в противном случае.
-
 Структура «Человек».
 Минимальный набор полей: фамилия, имя, пол, рост, вес, дата рождения, телефон, адрес.
 Добавление: в конец (стек)
 Удаление: с конца (стек)
 Поиск по фамилии, фильтр по дате рождения.
-
-4 Выделите память для списка динамически (используя указатель на базовый класс), а затем, в конце работы кода, освободите её. 
-	-Проверьте правильную работу деструкторов базового и производного классов (используйте виртуальный деструктор). 
-	-Преобразуйте указатель на базовый класс в указатель типа производного класса с помощью оператора dynamic_cast. 
+4 Выделите память для списка динамически (используя указатель на базовый класс), а затем, в конце работы кода, освободите её.
+	-Проверьте правильную работу деструкторов базового и производного классов (используйте виртуальный деструктор).
+	-Преобразуйте указатель на базовый класс в указатель типа производного класса с помощью оператора dynamic_cast.
 	-Проверьте работу деструкторов в случае выполнения операции освобождения памяти для преобразованного указателя.
-
-5 Написать функции сохранения списка файл* save() и извлечения списка из файла* load(). 
+5 Написать функции сохранения списка файл* save() и извлечения списка из файла* load().
 	-Для ввода/вывода элемента списка в/из файла используйте переопределённые функции ввода/вывода в поток (сделайте различие между вводом/выводом в консоль и вводом/выводом в файл).
-
 6 Введите манипулятор, форматирующий вывод, и примените его при выводе содержимого контейнера (таблица 2).
-
 Вывод в научном виде, ширина поля – 5 символов, 2 знака после запятой
 */
 
 using namespace std;
 
-std::ostream& operator>>(std::ostream& s,const string& hz)
+std::ostream& operator>>(std::ostream& s, const string& hz)
 {
 	return s >> hz;
 }
@@ -86,7 +79,7 @@ public:
 		, name = ("Роман")//имя 
 		, adres = ("Столовка физтеха") //адрес
 		, telephone = ("Samsung galaxy A52");//телефон
-	
+
 	date data; //дата рождения: день.месяц.год
 
 	bool sex = 0; //пол: 0-fmale, 1-male
@@ -106,10 +99,10 @@ public:
 		height = 176;
 	}
 
-	Human(string S_name , string name, bool sex ,double height , double weight , date data, string telephone, string adres) :
+	Human(string S_name, string name, bool sex, double height, double weight, date data, string telephone, string adres) :
 		Surname(S_name), name(name), sex(sex), height(height), weight(weight), data(data), telephone(telephone), adres(adres) {}
 
-	Human(const Human& hmn) : 
+	Human(const Human& hmn) :
 		Surname(hmn.Surname), name(hmn.name), sex(hmn.sex), height(hmn.height), weight(hmn.weight), data(hmn.data), telephone(hmn.telephone), adres(hmn.adres) {}
 
 	Human& operator=(const Human& hmn)
@@ -124,17 +117,14 @@ public:
 			data = hmn.data;
 			telephone = hmn.telephone;
 			adres = hmn.adres;
-		}
-		else
-		{
+		}	
 			return *this;
-		}
 	}
 
 
 	friend ostream& operator<<(ostream& s, const Human& hmn)
 	{
-		s << "\nФамилия ->" <<hmn.Surname << "\nИмя ->" << hmn.name << "\n.Пол ->" << hmn.sex << "\nРост ->" << hmn.height << "\nВес ->" << hmn.weight << "\nДата ->" << hmn.data 
+		s << "\nФамилия ->" << hmn.Surname << "\nИмя ->" << hmn.name << "\n.Пол ->" << hmn.sex << "\nРост ->" << hmn.height << "\nВес ->" << hmn.weight << "\nДата ->" << hmn.data
 			<< "\nТелефон ->" << hmn.telephone << "\nАдрес ->" << hmn.adres;
 		return s;
 	}
@@ -177,7 +167,6 @@ public:
 		info = el.info;
 	}
 
-	template<class T>
 	friend ostream& operator<<(ostream& fout, Element<T>& el)
 	{
 		fout << el.info;
@@ -190,10 +179,10 @@ class LinkedList
 {
 protected:
 
-		Element<T>* head;
-		Element<T>* tail;
-	
-		int count;
+	Element<T>* head;
+	Element<T>* tail;
+
+	int count;
 public:
 
 	LinkedList()
@@ -213,11 +202,11 @@ public:
 		return *current;
 	}
 
-	virtual bool isEmpty() 
+	virtual bool isEmpty()
 	{
 		return (LinkedList<T>::count == 0);
 	}
-	virtual int get_count() 
+	virtual int get_count()
 	{
 		return this->count;
 	}
@@ -229,15 +218,15 @@ public:
 	{
 		return this->tail;
 	}
-	
+
 	template<class T2>
 	friend ostream& operator<<(ostream& fout, LinkedList<T2>& el)
 	{
 		Element<T2>* current;
-		
-		for (current = el.head; current != nullptr; current = current -> next)
+
+		for (current = el.head; current != nullptr; current = current->next)
 			fout << *current << "; ";
-		
+
 		return fout;
 	}
 
@@ -254,7 +243,7 @@ public:
 	virtual ~LinkedList()
 	{
 		Element<T>* current = this->head;
-		
+
 		while (current != nullptr)
 		{
 			Element<T>* to_delete = current;
@@ -272,23 +261,23 @@ class Stack : public LinkedList<T>
 {
 public:
 	Stack<T>() : LinkedList<T>() {}
-	
-	Stack<T>(const Stack& stk) 
+
+	Stack<T>(const Stack& stk)
 	{
 		this->count = 0;
 
 		Element<T>* current = stk.head;
 		Element<T>* cur = nullptr;
-		
-		while (current != nullptr) 
+
+		while (current != nullptr)
 		{
-			if (cur == nullptr) 
+			if (cur == nullptr)
 			{
 				cur = new Element<T>(*current);
 				this->head = cur;
 				current = current->next;
 			}
-			else 
+			else
 			{
 				cur->next = new Element<T>(*current);
 				cur->next->prev = cur;
@@ -299,7 +288,47 @@ public:
 		}
 		this->tail = cur;
 	}
-	
+
+	Stack& operator=(const Stack& stk) 
+	{
+		if (this != &stk)
+		{
+			while (!this->isEmpty())
+			{
+				Element<T>* ptr = this->pop();
+				delete ptr;
+			}
+
+			this->count = 0;
+
+			Element<T>* current = stk.head;
+			Element<T>* current_this = nullptr;
+
+			while (current != nullptr)
+			{
+				if (current_this == nullptr)
+				{
+					current_this = new Element<T>(*current);
+					
+					this->head = current_this;
+					
+					current = current->next;
+				}
+				else
+				{
+					current_this->next = new Element<T>(*current);
+					current_this->next->prev = current_this;
+					current_this = current_this->next;
+					
+					current = current->next;
+				}
+				++(this->count);
+			}
+			this->tail = current_this;
+		}
+		return *this;
+	}
+
 	virtual Element<T>* push(T& value)
 	{
 		if (this->isEmpty())
@@ -309,7 +338,7 @@ public:
 			this->head->next = nullptr;
 			this->head->prev = nullptr;
 		}
-		else 
+		else
 		{
 			this->tail->next = new Element<T>(value);
 			this->tail->next->prev = this->tail;
@@ -318,47 +347,47 @@ public:
 		++(this->count);
 		return this->tail;
 	}
-	
+
 	virtual Element<T>* pop()
 	{
-		if (this->isEmpty()) 
+		if (this->isEmpty())
 		{
 			return nullptr;
 		}
 
 		Element<T>* answ = this->tail;
 
-		if (this->count == 1) 
+		if (this->count == 1)
 		{
 			this->head = nullptr;
 			this->tail = nullptr;
 		}
-		else 
+		else
 		{
 			this->tail = this->tail->prev;
 			this->tail->next = nullptr;
 		}
 		--(this->count);
-		
+
 		answ->prev = nullptr;
-		
+
 		return answ;
 	}
-	
-	Element<T>* insert(const T& value, int index) 
+
+	Element<T>* insert(const T& value, int index)
 	{
-		if (index <= this->count) 
+		if (index <= this->count)
 		{
 			Element<T>* current = this->head;
 			int current_num = 0;
 
-			while (current_num < index) 
+			while (current_num < index)
 			{
 				current = current->next;
 				++current_num;
 			}
 
-			if (current == this->head) 
+			if (current == this->head)
 			{
 				Element<T>* old = this->head;
 				this->head = new Element<T>(value);
@@ -367,7 +396,7 @@ public:
 				++(this->count);
 				return this->head;
 			}
-			else if (current != nullptr) 
+			else if (current != nullptr)
 			{
 				Element<T>* old_prev = current->prev;
 
@@ -378,7 +407,7 @@ public:
 				++(this->count);
 				return current->prev;
 			}
-			else 
+			else
 			{
 				this->tail->next = new Element<T>(value);
 				this->tail->next->prev = this->tail;
@@ -389,14 +418,14 @@ public:
 		}
 	}
 
-	Element<T> remove(int index) 
+	Element<T> remove(int index)
 	{
-		if (index < this->count) 
+		if (index < this->count)
 		{
 			Element<T>* current = this->head;
 			int current_num = 0;
 
-			while (current_num != index) 
+			while (current_num != index)
 			{
 				current = current->next;
 				++current_num;
@@ -404,20 +433,20 @@ public:
 			Element<T>* c_prev = current->prev;
 			Element<T>* c_next = current->next;
 
-			if (current == this->head) 
+			if (current == this->head)
 			{
 				this->head = this->head->next;
 			}
-			if (current == this->tail) 
+			if (current == this->tail)
 			{
 				this->tail = this->tail->prev;
 			}
 
 			Element<T> answ(*current);
-			answ.next = nullptr; 
+			answ.next = nullptr;
 			answ.prev = nullptr;
 
-			delete current; 
+			delete current;
 			current = nullptr;
 
 			if (c_prev != nullptr)
@@ -436,15 +465,15 @@ public:
 	}
 
 	template<typename T2>
-	Stack filter(bool (*value)(const T& vs, const T2 value), const T2 id) 
+	Stack filter(bool (*value)(const T& vs, const T2 value), const T2 id)
 	{
 		Stack<T> answ;
 
 		Element<T>* current = this->head;
-		
-		while (current != nullptr) 
+
+		while (current != nullptr)
 		{
-			if (value(current->info, id)) 
+			if (value(current->info, id))
 			{
 				answ.push(current->info);
 			}
@@ -454,22 +483,22 @@ public:
 	}
 
 	template<typename T2>
-	Stack<T>* filter_recursive(bool (*value)(const T& vs, const T2 value), const T2 id, Element<T>* begin = nullptr, Stack<T>* begin_start = nullptr) 
+	Stack<T>* filter_recursive(bool (*value)(const T& vs, const T2 value), const T2 id, Element<T>* begin = nullptr, Stack<T>* begin_start = nullptr)
 	{
-		if (begin_start == nullptr && begin == nullptr) 
+		if (begin_start == nullptr && begin == nullptr)
 		{
 			begin = this->head;
 			begin_start = new Stack<T>;
-		} 
-		else if (begin_start == nullptr) 
+		}
+		else if (begin_start == nullptr)
 		{
 			begin_start = new Stack<T>;
 		}
-		else if (begin == nullptr) 
+		else if (begin == nullptr)
 		{
 			return begin_start;
 		}
-		else if (value(begin->info, id)) 
+		else if (value(begin->info, id))
 		{
 			begin_start->push(begin->info);
 		}
@@ -477,11 +506,11 @@ public:
 		return filter_recursive(value, id, begin->next, begin_start);
 	}
 
-	void save(ofstream& fout) 
+	void save(ofstream& fout)
 	{
 		fout << this->count << endl;
 		Element<T>* current = this->head;
-		for (int i = 0; i < this->count; ++i) 
+		for (int i = 0; i < this->count; ++i)
 		{
 			fout << (*current) << endl;
 			current = current->next;
@@ -489,9 +518,9 @@ public:
 	}
 
 	template<typename T2>
-	void load(ifstream& fin) 
+	void load(ifstream& fin)
 	{
-		while (!this->isEmpty()) 
+		while (!this->isEmpty())
 		{
 			Element<T>* ptr = this->pop();
 			delete ptr;
@@ -499,11 +528,11 @@ public:
 
 		int vle = 0;
 		string str;
-		
+
 		getline(fin, str); //считываем всю строку , т.к. getline() нормально обрабатывает пробелы
 		std::istringstream foo(str); foo >> vle;
-		
-		for (int i = 0; i < vle; ++i) 
+
+		for (int i = 0; i < vle; ++i)
 		{
 			T2 current;
 			fin >> current;
@@ -512,19 +541,19 @@ public:
 
 	}
 
-	void print() 
+	void print()
 	{
-		while (!this->isEmpty()) 
+		while (!this->isEmpty())
 		{
 			Element<T>* ptr = this->pop();
-		
+
 			cout << ptr->info << "\n";
-			
+
 			delete ptr;
 		}
 	}
 
-	virtual ~Stack() 
+	virtual ~Stack()
 	{
 		Element<T>* current = this->head;
 
@@ -541,11 +570,11 @@ public:
 };
 
 bool surname_search(const Human& hmn, const string str = "") {
-	if (hmn.Surname == str) 
+	if (hmn.Surname == str)
 	{
 		return 1;
 	}
-	else 
+	else
 	{
 		return 0;
 	}
@@ -553,7 +582,7 @@ bool surname_search(const Human& hmn, const string str = "") {
 
 bool birthday(const Human& p, date& data) //я знаю, что это так не работает
 {
-	if (p.data.day <= data.day && data.mounth <= data.mounth && data.years <=data.years) 
+	if (p.data.day <= data.day && data.mounth <= data.mounth && data.years <= data.years)
 	{
 		return 1;
 	}
@@ -568,7 +597,7 @@ int main()
 	try
 	{
 		Stack<Human> hmn;
-		Human who,is,me;
+		Human who, is, me;
 		is.Surname = "Радужника";
 		me.Surname = "Икзарена";
 		hmn.push(who);
@@ -577,14 +606,14 @@ int main()
 
 		hmn.remove(1);
 
-		hmn.insert(is,1);
+		hmn.insert(is, 1);
 
-		Stack<Human>* test = hmn.filter_recursive(surname_search,string("Крейнин"));
+		Stack<Human>* test = hmn.filter_recursive(surname_search, string("Крейнин"));
 		Stack<Human> tesT = hmn.filter(surname_search, string("Крейнин"));
 
 		hmn.print();
 		test->print();
-	
+
 	}
 	catch (...)
 	{
@@ -612,38 +641,38 @@ int main()
 		int height = 176, weight = 65;
 		date data{ 12,12,2002 };
 
-		Human people(sname, name, keks, height, weight, data,telephone, adres);
+		Human people(sname, name, keks, height, weight, data, telephone, adres);
 
-		inf.push(people); 
-		inf.push(who); 
+		inf.push(people);
+		inf.push(who);
 		inf.push(who);
 
 		ofstream fout("text.txt");
 
-		if (fout.is_open()) 
+		if (fout.is_open())
 		{
 			inf.save(fout);
 			fout.close();
 		}
-		else 
+		else
 		{
 			std::cout << "file not opened\n";
 		}
 
 		ifstream fin("text.txt");
 
-		if (fin.is_open()) 
+		if (fin.is_open())
 		{
 			of.load(fin);
 			fin.close();
 			of.print();
 		}
-		else 
+		else
 		{
 			std::cout << "file not opened\n";
 		}
 
-		
+
 
 	}
 	catch (...)
